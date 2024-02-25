a45f63dfad5835f9ba8b43b061be46cfe5be91b4
Ryan Soares <134642662+RyanSoar3s@users.noreply.github.com>
2024-02-25 13:30:45 -0300

    Conteúdo do main atualizado e adicão de script JS

---
+++ src/scripts/show-contents.js
@@ -0,0 +1,8 @@
+let img_thumb = document.getElementById("img-thumb")
+let video = document.getElementsByClassName("video")[0]
+
+const showVideo = () => {
+    img_thumb.style.display = "none"
+    video.style.display = "block"
+
+}
