76924f0d909e8f0f75067d878ed0648288f0dcdb
Ryan Soares <134642662+RyanSoar3s@users.noreply.github.com>
2024-03-25 11:19:55 -0300

    validação de dados concluída

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,28 +1,74 @@
 // PATTERNS
-const EMAIL_PATTERN = /^[\w\.]+@gmail\.com$/
-const PASSWORD_PATTERN = /^[(A-Z)+(a-z)+(0-9)+(@\._)+]{8,}$/
-///^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@._])[\w\.]{8,}/
+const EMAIL_PATTERN = /^(?!gmail|email|outlook)\w+(?:\.\w+)*@[a-z]+(?:-[a-z]+)*\.[a-z]{2,}(?:\.[a-z]{2,})?$/
+const PASSWORD_PATTERN = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#\-&_.!\"'*]).{8,50}$/
 
 // VALIDATION
 document.querySelector("form").addEventListener("submit", (event) => {
     // DATAS
-    let email = document.querySelector('[data-email="email"]').value
-    let password = document.querySelector('[data-password="password"]').value
+    let input_email = document.querySelector('[data-email="email"]')
+    let input_password = document.querySelector('[data-password="password"]')
+    let email = input_email.value
+    let password = input_password.value 
+
+    //Error Message
+    let error_email = document.getElementsByClassName("invalid-data")[0]
+    let error_password = document.getElementsByClassName("invalid-data")[1]
 
     if (email == "" || password == "") {
-        alert("Por favor, digite em todos o campos.")
+        if (email == "") {
+            input_email.style.outline = "2px solid red"
+            error_email.textContent = "Campo vazio"
+
+        }
+
+        else {
+            input_email.style.outline = "none"
+            error_email.textContent = ""
+
+        }
+
+        if (password == "") {
+            input_password.style.outline = "2px solid red"
+            error_password.style.fontSize = "16px"
+            error_password.textContent = "Campo vazio"
+        
+        }
+
+        else {
+            input_password.style.outline = "none"
+            error_password.textContent = ""
+
+        }
+
         event.preventDefault()
 
     }
 
     else if (!email.match(EMAIL_PATTERN)) {
-        alert("Por favor, digite um email(gmail) válido.")
+        input_email.style.outline = "2px solid red"
+        error_email.textContent = "Por favor, digite um email válido."
+        input_password.style.outline = "none"
+        error_password.textContent = ""
+        event.preventDefault()
+
+    }
+
+    else if (password.length < 8 || password.length > 50) {
+        input_email.style.outline = "none"
+        error_email.textContent = ""
+        input_password.style.outline = "2px solid red"
+        error_password.style.fontSize = "16px"
+        error_password.textContent = "Por favor, a senha deve ter entre 8 e 50 caracteres."
         event.preventDefault()
 
     }
 
     else if (!password.match(PASSWORD_PATTERN)) {
-        alert("Por favor, digite uma senha com no mínimo 8 caracteres.")
+        input_email.style.outline = "none"
+        error_email.textContent = ""
+        input_password.style.outline = "2px solid red"
+        error_password.style.fontSize = "12px"
+        error_password.textContent = "Por favor, digite pelo menos: uma letra minúscula, uma letra maiúscula, um número e um caractere especial (@#-&_.!\"'*)"
         event.preventDefault()
 
     }
